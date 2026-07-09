997a990a0ee32c81a5cd4f2b08f1a38343a83ba4
Denis <denchic123@yandex.ru>
2026-07-09 13:02:16 +0300

    first try version

--- locations_data.js
+++ locations_data.js
@@ -1,35 +1,45 @@
 const LOCATIONS_DATA = {
         "series": [
             {
-                "id": 1,
-                "name": "Игра 1: Достопримечательности",
+                "id": 2,
+                "name": "очевидное невероятное",
                 "rounds": [
                     {
-                        "image": "location_s01_i01.png",
-                        "x": 4500,
-                        "y": 3800
+                        "image": "location_s02_i01.png",
+                        "x": 4198,
+                        "y": 12238
                     },
                     {
-                        "image": "location_s01_i02.png",
-                        "x": 7800,
-                        "y": 11800
+                        "image": "location_s02_i02.png",
+                        "x": 8117,
+                        "y": 10393
                     },
                     {
-                        "image": "location_s01_i03.png",
-                        "x": 2100,
-                        "y": 6400
+                        "image": "location_s02_i03.png",
+                        "x": 6594,
+                        "y": 6988
                     },
                     {
-                        "image": "location_s01_i04.png",
-                        "x": 9300,
-                        "y": 8200
+                        "image": "location_s02_i04.png",
+                        "x": 9775,
+                        "y": 5308
                     },
                     {
-                        "image": "location_s01_i05.png",
-                        "x": 14200,
-                        "y": 4100
+                        "image": "location_s02_i05.png",
+                        "x": 7214,
+                        "y": 6755
+                    },
+                    {
+                        "image": "location_s02_i06.png",
+                        "x": 8667,
+                        "y": 10642
                     }
                 ]
+            },
+            {
+                "id": 3,
+                "name": "Новая серия 3",
+                "rounds": []
             }
         ]
     };
